12f2e4f3da94ba6f44f45494cf580c303feccc00
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2024-10-13 00:23:35 +0530

    Add files via upload

---
+++ Payment gateway/demo/src/main/java/payment_gateway/demo/PaymentGatewayServicesApplication.java
@@ -0,0 +1,13 @@
+package payment_gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayServicesApplication.class, args);
+	}
+
+}
