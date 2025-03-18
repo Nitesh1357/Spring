21c19a44fba60053fc055ddaf16376121d7d62d1
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-03-18 21:09:38 +0530

    Create RedisConfiguration.java

---
+++ OTP-service/src/main/java/com/zap/payment/otp/service/configuration /RedisConfiguration.java
@@ -0,0 +1,111 @@
+package com.zap.payment.otp.service.configuration;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfiguration {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Value("${redis.ttl.minutes}")
+    private int redisTTLMinutes;
+
+    @Bean(value = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setConnectionFactory(factory);
+        return template;
+    }
+
+    @Primary
+    @Bean(name = "cacheManager")
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        Duration expiration = Duration.ofMinutes(redisTTLMinutes);
+        ObjectMapper mapper = JsonMapper.builder().configure(MapperFeature.USE_ANNOTATIONS,false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).build();
+        mapper.activateDefaultTyping(mapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        RedisCacheManager redisCacheManager =
+                RedisCacheManager.builder(redisConnectionFactory)
+                        .cacheDefaults(
+                                RedisCacheConfiguration.defaultCacheConfig()
+                                        .disableCachingNullValues()
+                                        .serializeValuesWith(
+                                                RedisSerializationContext.SerializationPair.fromSerializer(
+                                                        new GenericJackson2JsonRedisSerializer(mapper)))
+                                        .entryTtl(expiration))
+                        .build();
+        redisCacheManager.setTransactionAware(false);
+        return redisCacheManager;
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    ClientResources clientResources() {
+        return DefaultClientResources.create();
+    }
+
+    @Bean
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        return new RedisStandaloneConfiguration(redisHost, redisPort);
+    }
+
+    @Bean
+    public ClientOptions clientOptions(){
+        return ClientOptions.builder()
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .autoReconnect(true)
+                .build();
+    }
+
+    @Bean
+    LettucePoolingClientConfiguration lettucePoolConfig(ClientOptions options, ClientResources dcr){
+        return LettucePoolingClientConfiguration.builder()
+                .poolConfig(new GenericObjectPoolConfig())
+                .clientOptions(options)
+                .clientResources(dcr)
+                .build();
+    }
+
+    @Bean
+    public RedisConnectionFactory connectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration,
+                                                    LettucePoolingClientConfiguration lettucePoolConfig) {
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettucePoolConfig);
+    }
+
+}
