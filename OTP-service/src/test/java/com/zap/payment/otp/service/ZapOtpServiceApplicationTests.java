dd42155567cec25638082131a332dd430de0c8e5
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-04-22 21:00:27 +0530

    Create ZapOtpServiceApplicationTests.java

---
+++ OTP-service/src/test/java/com/zap/payment/otp/service/ZapOtpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zap.payment.otp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapOtpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
