00fcf609a3d2594e12d930b1d06f5559cce300d6
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-06-09 10:59:03 +0530

    Add files via upload

---
+++ springCurdDemo/demo/src/test/java/com/restapi_curddemo/demo/SpringRestApiCurdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi_curddemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiCurdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
