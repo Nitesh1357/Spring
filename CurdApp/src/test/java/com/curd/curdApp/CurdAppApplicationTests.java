7f517e2b6014cfe67b983bf7b7f3c2bd8cd20af1
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-07-07 13:07:28 +0530

    Add files via upload

---
+++ CurdApp/src/test/java/com/curd/curdApp/CurdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curd.curdApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
